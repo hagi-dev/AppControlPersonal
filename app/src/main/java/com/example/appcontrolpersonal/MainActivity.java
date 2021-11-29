8af15956080e8d46873d0692d8ed5ff12daaa830
hagi torres <hagiraitorresmacedo@gmail.com>
2021-11-28 23:14:17 -0500

    iniciando app control personal

---
+++ app/src/main/java/com/example/appcontrolpersonal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.appcontrolpersonal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
