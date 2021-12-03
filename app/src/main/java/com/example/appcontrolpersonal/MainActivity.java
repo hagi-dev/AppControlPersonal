2b5a73c28138501aecce1203d71d6f645d475aa2
hagi torres <hagiraitorresmacedo@gmail.com>
2021-12-03 16:15:01 -0500

    avance tipo trabajador

--- app/src/main/java/com/example/appcontrolpersonal/MainActivity.java
+++ app/src/main/java/com/example/appcontrolpersonal/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.listas_personal_desing);
+        setContentView(R.layout.lista_tipo_trabajador_desing);
     }
 }
